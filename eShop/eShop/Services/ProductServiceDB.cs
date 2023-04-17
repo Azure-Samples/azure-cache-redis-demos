5de146543cd62bc5348292533ef02becfe6b4902
Catherine Wang <cawa@microsoft.com>
2023-04-17 11:55:50 -0700

    adding eShop demo

---
+++ eShop/eShop/Services/ProductServiceDB.cs
@@ -0,0 +1,45 @@
+﻿using eShop.Data;
+using eShop.Interfaces;
+using eShop.Models;
+
+namespace eShop.Services
+{
+    public class ProductServiceDB : IProductService
+    {
+        private readonly eShopContext _context;
+
+        public ProductServiceDB(eShopContext context) 
+        {  
+            _context = context; 
+        }
+
+        public Task AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProrduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Product>> GetAllProductsAsync()
+        {
+            if (_context.Product == null) throw new Exception("Entity set 'eShopContext.Product'  is null.");
+            return await Task.Run(() => _context.Product.ToList());
+        }
+
+        public async Task<Product?> GetProductByIdAsync(int productId)
+        {
+            var product = await Task.Run(() => _context.Product.Where(product => product.Id == productId).FirstOrDefault());
+
+            return product;
+            
+        }
+    }
+}
