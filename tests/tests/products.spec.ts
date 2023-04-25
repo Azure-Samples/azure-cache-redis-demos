1d881025da4b14bdd81eb7df44e63ef615061e87
Marcus Felling <marcus.felling@gmail.com>
2023-04-25 10:19:28 -0500

    Use CSV to store product ID's

--- tests/tests/products.spec.ts
+++ tests/tests/products.spec.ts
@@ -1,18 +1,26 @@
 import { test, expect } from '@playwright/test';
+import fs from 'fs';
+import path from 'path';
+import { parse } from 'csv-parse/sync';
 
-test.describe('Product Details', () => {
-    // Loop through all products, view details, and add to cart
-    // NOTE: expect product 5 to fail as it does not exist
-    for (let i = 1; i <= 22; i++) {
-        test(`should be able to view product details for product ${i}`, async ({ page }) => {
-            await page.goto(`/Home/Details/${i}`);
-            await expect(page).toHaveURL(`/Home/Details/${i}`);
+test.describe('Product Details', () => {  
+    // Use product ID's from CSV   
+    const products = parse(fs.readFileSync(path.join(__dirname, 'test-data.csv')), {
+        columns: true,
+        skip_empty_lines: true
+    }); 
+
+    // Loop through all productID's, view details, and add to cart
+    for (const ID in products) {
+        test(`should be able to view product details for product ${ID}`, async ({ page }) => {
+            await page.goto(`/Home/Details/${ID}`);
+            await expect(page).toHaveURL(`/Home/Details/${ID}`);
         });
 
-        test(`should be able to add product ${i} to cart`, async ({ page }) => {
-            await page.goto(`/Home/Details/${i}`);
-            await page.getByRole('link', { name: 'Shopping Cart' }).click();
-            await expect(page).toHaveURL(`/Carts`);
+        test(`should be able to add product ${ID} to cart`, async ({ page }) => {
+            await page.goto(`/Home/Details/${ID}`);
+            await page.getByRole('button', { name: 'Add to cart' }).click();
+            await expect(page).toHaveURL(`/Carts/Create`);
         });
     }
 });
