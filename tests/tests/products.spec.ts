ba85b6e3fa40768a06d2611007b4212b85952b1c
Marcus Felling <marcus.felling@gmail.com>
2023-04-24 20:05:25 -0500

    product id 5 missing

--- tests/tests/products.spec.ts
+++ tests/tests/products.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Product Details', () => {
+    // Loop through all products, view details, and add to cart
+    // NOTE: expect product 5 to fail as it does not exist
     for (let i = 1; i <= 22; i++) {
         test(`should be able to view product details for product ${i}`, async ({ page }) => {
             await page.goto(`/Home/Details/${i}`);
