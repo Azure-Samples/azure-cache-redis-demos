22738c755f21081ebc4e1155017cdd8daf5c4ec4
Marcus Felling <marcus.felling@gmail.com>
2023-04-26 09:42:02 -0500

    updates to meaasure performance

--- tests/tests/shopping-cart.spec.ts
+++ tests/tests/shopping-cart.spec.ts
@@ -14,7 +14,7 @@ test.describe('Shopping Cart', () => {
 });
 
 test.describe('Shopping Cart - Performance', () => {
-    test('should be able to add items to cart and measure shopping cart performance', async ({ page }) => {
+    test('should be able to add items to cart and measure shopping cart performance', async ({ page }, TestInfo) => {
         // Product ID's to test
         const products = process.env.PRODUCTIDS.split(',');
         // Loop through each product and add to cart
@@ -22,11 +22,18 @@ test.describe('Shopping Cart - Performance', () => {
             await page.goto(`/Home/Details/${i}`);
             await page.getByRole('button', { name: 'Add to cart' }).click();
         }
-        // Navigate to shopping cart and measure performance
-        await page.goto(`/Cart`);
-        const performanceTimingJson = await page.evaluate(() => JSON.stringify(window.performance.timing))
-        const performanceTiming = JSON.parse(performanceTimingJson)
-        const startToInteractive = performanceTiming.domInteractive - performanceTiming.navigationStart
-        test.info().annotations.push({ type: 'Performance', description: `Navigation start to DOM interactive: ${startToInteractive}ms` });
+        // Navigate to shopping cart
+        await page.goto(`/Carts`);
+        // Use Performance API to measure performance 
+        // https://developer.mozilla.org/en-US/docs/Web/API/Performance/getEntriesByType
+        const performance = await page.evaluate(() => performance.getEntriesByType('navigation'));
+        // Get the first entry
+        const performanceTiming = performance[0];
+        // Get the start to load event end time
+        const startToLoadEventEnd = performanceTiming.loadEventEnd - performanceTiming.startTime;
+        // Add the performance annotation to the HTML report
+        test.info().annotations.push({ type: 'Performance', description: `${TestInfo.project.use.baseURL} - Navigation start to load event end for ${TestInfo.project.name}: ${startToLoadEventEnd}ms` });
+        // Also output to console for debugging
+        console.log(`${TestInfo.project.use.baseURL} - Navigation start to load event end for ${TestInfo.project.name}: ${startToLoadEventEnd}ms`);
     });
 });
