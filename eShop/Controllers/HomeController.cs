e253e4c397a502aea5186a6d4e8fab84ace2fb09
Catherine Wang <cawa@microsoft.com>
2023-05-31 12:18:28 -0700

    removing page load time

--- eShop/Controllers/HomeController.cs
+++ eShop/Controllers/HomeController.cs
@@ -80,10 +80,7 @@ namespace eShop.Controllers
 
         public async Task<IActionResult> Details(int id)
         {
-            Stopwatch sw = Stopwatch.StartNew();
             Product _product = await _productService.GetProductByIdAsync(id);
-            sw.Stop();
-            double ms = sw.ElapsedTicks / (Stopwatch.Frequency / (1000.0));
 
             if (_product == null)
             {
@@ -91,8 +88,6 @@ namespace eShop.Controllers
             }
 
             HttpContext.Session.SetInt32(SessionConstants.LastViewed, id);
-
-            ViewData["pageLoadTime"] = ms;
 
             return View(_product);
         }
